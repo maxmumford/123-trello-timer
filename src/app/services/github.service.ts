e59eaf090120e3e55becf2bb93fd06a354ac3b07
Max Mumford <maxmumford@gmail.com>
2017-12-13 12:14:35 +0000

    Improve version checker

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -5,9 +5,13 @@ import { ElectronService } from 'app/services/electron.service';
 
 var compareVersions = require('compare-versions');
 
+interface GithubAsset {
+  browser_download_url: string
+}
+
 interface GithubRelease {
   tag_name: string
-  zipball_url: string
+  assets: GithubAsset[]
 }
 
 interface Release { 
@@ -27,38 +31,22 @@ export class GithubService {
     const currentVersion = this.electronService.getVersion()
 
     // get releases from github
-    return this.http.get<GithubRelease[]>("https://api.github.com/repos/maxmumford/123-trello-timer/releases")
+    return this.http.get<GithubRelease>("https://api.github.com/repos/maxmumford/123-trello-timer/releases/latest")
 
         // map them onto our release object
-        .map(grs => {
-          return grs.map(gr => {
-            return {
-              version: gr.tag_name,
-              downloadUrl: gr.zipball_url
-            }
-          })
+        .map(gr => {
+          return {
+            version: gr.tag_name,
+            downloadUrl: gr.assets[0].browser_download_url
+          }
         })
 
         // extract latest release and return it
-        .map(releases => {
-          let latest = this.getLatestRelease(releases)
-          if(compareVersions(latest.version, currentVersion) >= 1)
-            return latest
+        .map(release => {
+          if(compareVersions(release.version, currentVersion) >= 1)
+            return release
           return null
         })
-  }
-
-  private getLatestRelease(releases: Release[]){
-    if(releases.length == 0)
-      return null
-    let sortedVersions = releases.map(r => r.version)
-        .sort(compareVersions)
-    let latestVersionNumber = sortedVersions[sortedVersions.length - 1]
-    for(let r of releases){
-      if(r.version == latestVersionNumber)
-        return r
-    }
-    return null
   }
 
 }
